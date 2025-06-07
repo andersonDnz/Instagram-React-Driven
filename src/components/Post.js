5b74e7320287a7368efb1ca26b922b00437672cc
anderson <andersondinizep@gmail.com>
2025-06-07 20:15:06 -0300

    feat:criando componente post.js

---
+++ src/components/Post.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+
+import { IonIcon } from '@ionic/react';
+
+
+
+import styles from "./Posts.module.css";
+
+import {
+  heartOutline,
+  heart,
+  chatbubbleOutline,
+  paperPlaneOutline,
+  bookmarkOutline,
+  bookmark,
+  ellipsisHorizontal,
+} from 'ionicons/icons';
+
+export default function Post({ post }) {
+  const [liked, setLiked] = useState(false);
+  const [saved, setSaved] = useState(false);
+  const [likes, setLikes] = useState(post.likes);
+
+  function toggleLike(byImage = false) {
+    if (liked && byImage) return;
+    if (liked) {
+      setLikes(likes - 1);
+      setLiked(false);
+    } else {
+      setLikes(likes + 1);
+      setLiked(true);
+    }
+  }
+
+  return (
+    <div className={styles.post}>
+      <div className={styles.topo}>
+        <div className={styles.usuario}>
+          <img src={post.userImg} alt={post.user} />
+          {post.user}
+        </div>
+        <div className={styles.acoes}>
+          <IonIcon icon={ellipsisHorizontal} />
+        </div>
+      </div>
+
+      <div className={styles.conteudo} onClick={() => toggleLike(true)}>
+        <img src={post.contentImg} alt="conteudo" />
+      </div>
+
+      <div className={styles.fundo}>
+        <div className={styles.acoes}>
+          <div>
+            <IonIcon
+              icon={liked ? heart : heartOutline}
+              color={liked ? 'danger' : ''}
+              onClick={toggleLike}
+            />
+            <IonIcon icon={chatbubbleOutline} />
+            <IonIcon icon={paperPlaneOutline} />
+          </div>
+          <div>
+            <IonIcon
+              icon={saved ? bookmark : bookmarkOutline}
+              onClick={() => setSaved(!saved)}
+            />
+          </div>
+        </div>
+
+        <div className={styles.curtidas}>
+          <img src={post.likedByImg} alt={post.likedBy} />
+          <div className={styles.texto}>
+            Curtido por <strong>{post.likedBy}</strong> e <strong>outras {likes.toLocaleString()} pessoas</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,13 +1,5 @@
-import React from 'react';
-import styles from "./Posts.module.css";
-import { IonIcon } from '@ionic/react';
-import {
-  ellipsisHorizontal,
-  heartOutline,
-  chatbubbleOutline,
-  paperPlaneOutline,
-  bookmarkOutline,
-} from 'ionicons/icons';
+import Post from './Post';
+
 
 import meowed from '../../src/assets/img/meowed.svg';
 import gatoTelefone from '../../src/assets/img/gato-telefone.svg';
@@ -16,73 +8,39 @@ import barked from '../../src/assets/img/barked.svg';
 import dog from '../../src/assets/img/dog.svg';
 import adorableAnimals from '../../src/assets/img/adorable_animals.svg';
 
+const postsData = [
+  {
+    user: 'meowed',
+    userImg: meowed,
+    contentImg: gatoTelefone,
+    likedBy: 'respondeai',
+    likedByImg: respondeai,
+    likes: 101523
+  },
+  {
+    user: 'barked',
+    userImg: barked,
+    contentImg: dog,
+    likedBy: 'adorable_animals',
+    likedByImg: adorableAnimals,
+    likes: 99159
+  },
+  {
+    user: 'meowed',
+    userImg: meowed,
+    contentImg: gatoTelefone,
+    likedBy: 'respondeai',
+    likedByImg: respondeai,
+    likes: 101523
+  }
+];
+
 export default function Posts() {
-  const posts = [
-    {
-      usuario: "meowed",
-      avatar: meowed,
-      imagem: gatoTelefone,
-      curtidoPor: "respondeai",
-      avatarCurtida: respondeai,
-      outrasCurtidas: "101.523"
-    },
-    {
-      usuario: "barked",
-      avatar: barked,
-      imagem: dog,
-      curtidoPor: "adorable_animals",
-      avatarCurtida: adorableAnimals,
-      outrasCurtidas: "99.159"
-    },
-    {
-      usuario: "meowed",
-      avatar: meowed,
-      imagem: gatoTelefone,
-      curtidoPor: "respondeai",
-      avatarCurtida: respondeai,
-      outrasCurtidas: "101.523"
-    }
-  ];
-
   return (
-    <div className={styles.post}>
-      {posts.map((post) =>
-        <div className={styles.post}>
-          <div className={styles.topo}>
-            <div className={styles.usuario}>
-              <img src={post.avatar} alt={post.usuario} />
-              {post.usuario}
-            </div>
-            <div className={styles.acoes}>
-              <IonIcon icon={ellipsisHorizontal} />
-            </div>
-          </div>
-
-          <div className={styles.conteudo}>
-            <img src={post.imagem} alt="conteúdo" />
-          </div>
-
-          <div className={styles.fundo}>
-            <div className={styles.acoes}>
-              <div>
-                <IonIcon icon={heartOutline} />
-                <IonIcon icon={chatbubbleOutline} />
-                <IonIcon icon={paperPlaneOutline} />
-              </div>
-              <div>
-                <IonIcon icon={bookmarkOutline} />
-              </div>
-            </div>
-
-            <div className={styles.curtidas}>
-              <img src={post.avatarCurtida} alt={post.curtidoPor} />
-              <div className={styles.texto}>
-                Curtido por <strong>{post.curtidoPor}</strong> e <strong>outras {post.outrasCurtidas} pessoas</strong>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+    <div>
+      {postsData.map((post, index) => (
+        <Post key={index} post={post} />
+      ))}
     </div>
   );
 }
